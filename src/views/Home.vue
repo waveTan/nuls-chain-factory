fed497ed8ee48d02af036a6a8f2f8da9058dd5d1
wave <986887991@qq.com>
2019-07-16 16:44:36 +0800

    初始模板完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="home w1200">
-    <div class="address">
-      <span>地址: {{addressInfo.address}}</span> <font> 余额：{{addressInfo.totalBalance/100000000}} NULS</font>
+  <div class="home">
+    <div class="carousel"></div>
+    <div class="w1200 bg-white">
+      <div class="left fl">
+        <p>
+          通过NULS链工厂，企业/个人不再需要深入钻研区块链底层技术。仅需进行简单的配置工作，就能得到一条完全属于自己的区块链。
+        </p>
+        <el-row>
+          <el-button>了解区块链架构</el-button>
+          <el-button type="success">前往搭建区块链</el-button>
+        </el-row>
+      </div>
+      <div class="right fr">
+
+      </div>
+    </div>
+    <div class="w1200 bg-gray">
+      <div class="left fl">
+
+      </div>
+      <div class="right fr">
+        <p>
+          微服务架构，支持根据实际业务定制化开发和现有模块进行结合。
+        </p>
+        <el-row>
+          <el-button type="success">前往搭建区块链</el-button>
+        </el-row>
+      </div>
     </div>
   </div>
 </template>
@@ -20,3 +45,37 @@
     methods: {}
   }
 </script>
+
+<style lang="less">
+  .home {
+    .carousel {
+      height: 400px;
+      width: 100%;
+      border-bottom: 1px solid #5e6983;
+    }
+    .w1200 {
+      height: 450px;
+      .left, .right {
+        width: 50%;
+        p {
+          font-size: 18px;
+          font-weight: bold;
+          line-height: 36px;
+          color: #000000;
+          width: 500px;
+          margin: 100px auto 70px;
+        }
+        .el-button {
+          display: block;
+          width: 200px;
+          height: 40px;
+          margin: 0 0 0 50px;
+        }
+        .el-button--success {
+          margin: 20px 0 0 50px;
+        }
+      }
+    }
+  }
+
+</style>
