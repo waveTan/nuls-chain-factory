2778177246f6d5307b7fa4a16dda67310c4e7b68
wave <986887991@qq.com>
2019-07-16 10:05:10 +0800

    初始

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home w1200">
+    <div class="address">
+      <span>地址: {{addressInfo.address}}</span> <font> 余额：{{addressInfo.totalBalance/100000000}} NULS</font>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {chainIdNumber} from '@/api/util'
+
+  export default {
+    data() {
+      return {
+        addressInfo: JSON.parse(localStorage.getItem(chainIdNumber())),//地址信息
+      };
+    },
+    created() {
+    },
+    methods: {}
+  }
+</script>
